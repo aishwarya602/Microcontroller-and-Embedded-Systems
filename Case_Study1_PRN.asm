74896ea196ccf68884541e702fc293e709a7b632
aishwarya602 <aishwaryabrahmankar06@gmail.com>
2026-02-05 08:19:51 +0530

    Add files via upload

---
+++ Case_Study1_PRN.asm
@@ -0,0 +1,12 @@
+MOV A, #10H       
+INC A             
+INC A             
+
+MOV B, #09H      
+MOV R0, #09H     
+ADD A, R0        
+
+MOV A, #09H       
+ADD A, #09H       
+MOV B, A          
+MOV A, #12H       
---
+++ Case_Study4_Mobile_no.asm
@@ -0,0 +1,16 @@
+MOV A, #0FFH      
+ANL A, #57H       
+
+ORL A, #02H   
+   
+ANL A, #0F2H   
+  
+MOV R1, A    
+     
+MOV A, #0AAH     
+CLR A 
+            
+MOV B, R1
+
+    
+     
