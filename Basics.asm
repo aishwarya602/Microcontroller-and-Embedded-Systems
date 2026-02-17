7a7ff96cf7d7a7fd0a42fc5268ceb2461033e7e4
aishwarya602 <aishwaryabrahmankar06@gmail.com>
2026-02-17 15:10:20 +0530

    Add files via upload

---
+++ Basics.asm
@@ -0,0 +1,11 @@
+Q1. Write a program to load any values in all 7 registers
+MOV A, #01H
+MOV B, #10H
+MOV R0, #11H
+MOV R1, #12H
+MOV R2, #13H
+MOV R3, #14H
+MOV R4, #15H
+MOV R5, #16H
+MOV R6, #17H
+MOV R7, #18H
